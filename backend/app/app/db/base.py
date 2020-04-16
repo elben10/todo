b1ddbb83c78960e6467dce02e5787de5927630c5
Jakob Jul Elben <jakob@datamaga.com>
2020-04-16 21:43:43 +0200

    Add user model

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -1,1 +1,2 @@
-from .base_class import Base
+from app.db.base_class import Base
+from app.models.user import User
---
+++ backend/app/app/models/user.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+
+from app.db.base_class import Base
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    fullname = Column(String, index=True)
+    email = Column(String, index=True, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True, index=True)
+    is_superuser = Column(Boolean, default=False, index=True)
+    signup_datetime = Column(DateTime, default=datetime.utcnow)
